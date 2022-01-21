d01bf115e1a2d01bcc19aeb1bbde83ffe5d59264
Jajasek <jachym.mierva@gmail.com>
2022-01-22 00:03:25 +0100

    Turbine chooser

--- src/load-components/machines.lua
+++ src/load-components/machines.lua
@@ -40,54 +40,140 @@ local function saveConfig(config)
   file:close()
 end
 
-local function chooseReactor()
+
+local function setReactor()
   local available = component.list('br_reactor')
   local n = len(available)
   if n == 0 then
     io.stderr:write('Error: no available reactor')
     return
   elseif n == 1 then
-    return component.proxy((available()))
+    local addr = (available())
+    return addr, component.proxy(addr)
   else
-    print(n..' reactors are available. Press [y] to choose currently activated reactor, anything else to activate another reactor.')
+    print(n..' reactors are available. Press [y] to choose the currently '
+           ..'activated reactor, or anything else to activate another reactor. '
+           ..'Press [q] to quit.')
     local i = 0
-    for addr, _ in pairs(available) do
-      local reactor = component.proxy(addr)
-      local rod_levels = {}
-      for j = 1, reactor.getNumberOfControlRods() do
-        table.insert(rod_levels, reactor.getControlRodLevel(j - 1))
-      end
-      reactor.setAllControlRodLevels(100)
-      reactor.setActive(true)
+    while true do
+      for addr, _ in pairs(available) do
+        local reactor = component.proxy(addr)
+        local rod_levels = {}
+        for j = 1, reactor.getNumberOfControlRods() do
+          table.insert(rod_levels, reactor.getControlRodLevel(j - 1))
+        end
+        reactor.setAllControlRodLevels(100)
+        reactor.setActive(true)
 
-      i = i + 1
-      print('['..i..'/'..n..']')
-      _, _, ch = event.pull('key_down')
+        i = i + 1
+        print('['..i..'/'..n..']')
+        _, _, ch = event.pull('key_down')
 
-      reactor.setActive(false)
-      for j = 1, reactor.getNumberOfControlRods() do
-        reactor.setControlRodLevel(j - 1, table.remove(rod_levels))
-      end
-      if string.lower(string.char(ch)) == 'y' then
-        return reactor
+        reactor.setActive(false)
+        for j = 1, reactor.getNumberOfControlRods() do
+          reactor.setControlRodLevel(j - 1, table.remove(rod_levels))
+        end
+        local cmd = string.lower(string.char(ch))
+        if cmd == 'y' then
+          return addr, reactor
+        elseif cmd == 'q' then
+          return
+        end
       end
     end
-    io.stderr:write('Error: no reactor was chosen')
+  end
+end
+
+local function setTurbines(partial)
+  local available = component.list('br_turbine')
+  local n = len(available)
+  if n == 0 then
+    io.stderr:write('Error: no available turbine')
+    return
+  elseif n == 1 then
+    local addr = (available())
+    return {addr}, {component.proxy(addr)}
+  else
+    print('There are '..n..' turbines. How many will be used?')
+    local count = tonumber(io.read())
+    if (not count) or count <= 0 or count > n then
+      io.stderr:write('Error: illegal value')
+      return
+    end
+    local t_addresses = {}
+    local t_proxies = {}
+    for i = 1, count do
+      print('Select '..i..'. turbine, there are '..(n - i + 1)..' turbines '
+          ..'available. Press [y] to choose the activated turbine, or anything '
+          ..'else to activate another turbine. Press [q] to quit.')
+      local j = 0
+      local selected = false
+      while not selected do
+        for addr, _ in pairs(available) do
+          local turbine = component.proxy(addr)
+          local flow_rate_max = turbine.getFluidFlowRateMax()
+          local coils_engaged = turbine.getInductorEngaged()
+          turbine.setFluidFlowRateMax(0)
+          turbine.setInductorEngaged(false)
+          turbine.setActive(true)
+
+          j = j + 1
+          print('['..j..'/'..(n - i + 1)..']')
+          _, _, ch = event.pull('key_down')
+
+          turbine.setActive(false)
+          turbine.setFluidFlowRateMax(flow_rate_max)
+          turbine.setInductorEngaged(coils_engaged)
+          local cmd = string.lower(string.char(ch))
+          if cmd == 'y' then
+            table.insert(t_addresses, addr)
+            table.insert(t_proxies, turbine)
+            available[addr] = nil
+            selected = true
+            break
+          elseif cmd == 'q' then
+            return
+          end
+        end
+      end
+    end
+    return t_addresses, t_proxies
   end
 end
 
 local function getConfig()
   local addresses = loadConfig()
   local proxies = {}
+  local save = false
   if not addresses then
     io.stderr:write('Error while trying to read configuration file')
     return
   end
+
   if (not addresses.reactor) or
           component.type(addresses.reactor) ~= 'br_reactor' then
-    proxies.reactor = chooseReactor()
+    addresses.reactor, proxies.reactor = setReactor()
+    save = true
   else
     proxies.reactor = component.proxy(addresses.reactor)
+  end
+
+  if addresses.turbines then
+    proxies.turbines = {}
+    for _, addr in ipairs(addresses.turbines) do
+      if component.type(addr) == 'br_turbine' then
+        table.insert(proxies.turbines, component.proxy(addr))
+      else
+        addresses.turbines, proxies.turbines = setTurbines(proxies.turbines)
+        break
+      end
+    end
+  else
+    addresses.turbines, proxies.turbines = setTurbines()
+  end
+
+  if save then
+    saveConfig(addresses)
   end
 end
 
