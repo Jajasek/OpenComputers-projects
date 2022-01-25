4578f603d72964d8ffe77f12f1cc74461e02dfa9
Jajasek <jachym.mierva@gmail.com>
2022-01-25 14:22:43 +0100

    reformat, use machines.lua

--- src/regulation/powerplant.lua
+++ src/regulation/powerplant.lua
@@ -1,5 +1,6 @@
-local r, t1, t2, t3, s1, s2, w = table.unpack(require('loadComp'))
-local t = {t1, t2, t3}
+-- local r, t1, t2, t3, s1, s2, w = table.unpack(require('loadComp'))
+-- local t = {t1, t2, t3}
+local machines = require('machines')
 local regulation = require('regulation')
 local event = require('event')
 local keyboard = require('keyboard')
@@ -9,20 +10,25 @@ local sides = require('sides')
 local target_t = {}
 local enable_coil = {}
 local INDUCTION_THRESHOLD = 64
-local NUMBER_OF_TURBINES = 3
 
-local target_r = 0.93  --the fraction of available steam storage space that we want to be filled
-local mode_r = 'balance'  --can be 'balance', 'standby' or 'stop'
-local mode_t = 'standby'  --can be 'standby', 'power_1', 'power_2', 'power_3', 'power_4' or 'stop'
-local MAX_CRL = 90  --when control rods are almost fully inserted, the reaction starts to weirdly fluctuate
+local target_r = 0.93  -- the fraction of available steam storage space that we
+                       -- want to be filled
+local mode_r = 'balance'  -- can be 'balance', 'standby' or 'stop'
+local mode_t = 'standby'  -- can be 'standby', 'power_1', 'power_2', 'power_3',
+                          -- 'power_4' or 'stop'
+local MAX_CRL = 90  -- when control rods are almost fully inserted, the reaction
+                    -- starts to weirdly fluctuate
 local WATER_EMERGENCY_STOP = 0.5
-local PRESSURE_THRESHOLD = 0.97  --enter standby mode. During very low steam consumption the pressure
-                                --oscillates around the target with a deviation of (PRESSURE_THRESHOLD - target_r).
+local PRESSURE_THRESHOLD = 0.97  --enter standby mode. During very low steam
+  -- consumption the pressure oscillates around the target with a deviation
+  -- of (PRESSURE_THRESHOLD - target_r).
 
 local EVENT_WAIT = 0.005
 local MODEM_PERIOD = 3
 local MODEM_INFO_PORT = 1
-local timer = event.timer(MODEM_PERIOD, function() modem.broadcast(MODEM_INFO_PORT, 't_targets', table.unpack(target_t)) end, math.huge)
+local timer = event.timer(MODEM_PERIOD, function()
+  modem.broadcast(MODEM_INFO_PORT, 't_targets', table.unpack(target_t))
+end, math.huge)
 
 local K_POWER1 = string.byte('1')
 local K_POWER2 = string.byte('2')
@@ -38,48 +44,54 @@ local K_GET_STATE = string.byte('g')
 local K_HELP = string.byte('h')
 
 local regulators_t = {}
-for i = 1, NUMBER_OF_TURBINES do
-  regulators_t[i] = regulation.PDM(
-    t[i].getRotorSpeed,
-    t[i].setFluidFlowRateMax,
+for _, turbine in ipairs(machines.turbines) do
+  table.insert(regulators_t, regulation.PDM(
+    turbine.getRotorSpeed,
+    turbine.setFluidFlowRateMax,
     0,
     0.03, 0.1,
     2000, false
-  )
+  ))
 end
 
 
-function get_pressure()
-  return (s1.getTankLevel(sides.top) + s2.getTankLevel(sides.top)) / 
-         (s1.getTankCapacity(sides.top) + s2.getTankCapacity(sides.top))
+local function get_pressure()
+  local amount = 0
+  local capacity = 0
+  for _, tank in ipairs(machines.steam) do
+    local info = tank[1].getFluidInTank(tank[2])[tank[3]]
+    amount = amount + (info.amount or 0)
+    capacity = capacity + (info.capacity or 0)
+  end
+  if capacity ~= 0 then return amount / capacity end
 end
 
 
 local regulator_r = regulation.PDM(
   get_pressure,
-  r.setAllControlRodLevels,
+  machines.reactor.setAllControlRodLevels,
   target_r, 0.8, 30,
   MAX_CRL, true
 )
 
 
-function init_reactor()
+local function init_reactor()
   local init_crl = 0
-  for i = 0, r.getNumberOfControlRods() - 1 do
-    init_crl = init_crl + r.getControlRodLevel(i)
+  for i = 0, machines.reactor.getNumberOfControlRods() - 1 do
+    init_crl = init_crl + machines.reactor.getControlRodLevel(i)
   end
-  regulator_r.init(init_crl / r.getNumberOfControlRods())
+  regulator_r.init(init_crl / machines.reactor.getNumberOfControlRods())
 end
 
 
-function change_mode_turbine(key)
+local function change_mode_turbine(key)
   if key == K_TURBINES_STOP or key == K_STOP then
     print('Turbines - STOP')
     mode_t = 'stop'
-    for i = 1, NUMBER_OF_TURBINES do
+    for i, turbine in ipairs(machines.turbines) do
       target_t[i] = 0
       enable_coil[i] = true
-      t[i].setActive(false)
+      turbine.setActive(false)
       regulators_t[i].set_setpoint(target_t[i])
     end
     return
@@ -112,16 +124,14 @@ function change_mode_turbine(key)
   else
     return
   end
-  t1.setActive(true)
-  t2.setActive(true)
-  t3.setActive(true)
-  for i = 1, NUMBER_OF_TURBINES do
+  for i, turbine in ipairs(machines.turbines) do
+    turbine.setActive(true)
     regulators_t[i].set_setpoint(target_t[i])
   end
 end
 
 
-function change_mode_reactor(key) 
+local function change_mode_reactor(key)
   if key == K_REACTOR_STOP or key == K_STOP then
     print('Reactor - STOP')
     mode_r = 'stop'
@@ -136,13 +146,13 @@ function change_mode_reactor(key)
 end
 
 
-function info(key)
+local function info(key)
   if key == K_GET_STATE then
     print('Current state:')
     print(string.format('  Turbines - %s', mode_t))
-    for i = 1, NUMBER_OF_TURBINES do
+    for i, turbine in machines.turbines do
       local state = 'engaged'
-      if not t[i].getInductorEngaged() then
+      if not turbine.getInductorEngaged() then
         state = 'dis' .. state
       end
       print(string.format('    Turbine %d - %s', i, state))
@@ -154,9 +164,9 @@ function info(key)
 end
 
 
-for i = 1, NUMBER_OF_TURBINES do
-  t[i].setActive(true)
-  regulators_t[i].init(t[i].getFluidFlowRateMax())
+for i, turbine in ipairs(machines.turbines) do
+  turbine.setActive(true)
+  regulators_t[i].init(turbine.getFluidFlowRateMax())
 end
 change_mode_turbine(K_TURBINES_STANDBY)
 change_mode_reactor(K_BALANCE)
@@ -167,11 +177,11 @@ while true do
     local _, _, key = event.pull(EVENT_WAIT, 'key_down')
     if key == 3 then
       print('interrupting...')
-      r.setActive(false)
-      for i = 1, NUMBER_OF_TURBINES do
-        t[i].setActive(false)
-        t[i].setInductorEngaged(false)
-        t[i].setFluidFlowRateMax(0)
+      machines.reactor.setActive(false)
+      for _, turbine in ipairs(machines.turbines) do
+        turbine.setActive(false)
+        turbine.setInductorEngaged(false)
+        turbine.setFluidFlowRateMax(0)
       end
       event.cancel(timer)
       os.exit()
@@ -183,10 +193,11 @@ while true do
   
   --regulate reactor
   --update mode_r based on current pressure
-  if (get_pressure() >= PRESSURE_THRESHOLD and mode_r == 'balance') then
+  if get_pressure() >= PRESSURE_THRESHOLD and mode_r == 'balance' then
     print('Reactor - standby')
     mode_r = 'standby'
-  elseif (get_pressure() <= (2*target_r) - PRESSURE_THRESHOLD and mode_r == 'standby') then
+  elseif get_pressure() <= (2*target_r) - PRESSURE_THRESHOLD and
+      mode_r == 'standby' then
     print('Reactor - balance')
     mode_r = 'balance'
   end
@@ -201,29 +212,32 @@ while true do
     --]]
   end
   --activate/deactivate reactor
-  if (r.getActive() and (mode_r == 'standby' or mode_r == 'stop')) then
-    r.setActive(false)
-  elseif (not r.getActive() and mode_r == 'balance') then
-    r.setActive(true)
+  if machines.reactor.getActive() and (mode_r == 'standby' or mode_r == 'stop')
+  then
+    machines.reactor.setActive(false)
+  elseif (not machines.reactor.getActive() and mode_r == 'balance') then
+    machines.reactor.setActive(true)
   end
   
   --regulate turbines
-  for i = 1, NUMBER_OF_TURBINES do
+  for i, turbine in ipairs(machines.turbines) do
     regulators_t[i]()
     
     --update coils based on current speed
-    local speed = t[i].getRotorSpeed()
+    local speed = turbine.getRotorSpeed()
     if not enable_coil[i] then
-      if t[i].getInductorEngaged() then
+      if turbine.getInductorEngaged() then
         print(string.format('Turbine %d - disengaged', i))
-        t[i].setInductorEngaged(false)
+        turbine.setInductorEngaged(false)
       end
-    elseif speed >= (target_t[i] - INDUCTION_THRESHOLD) and not t[i].getInductorEngaged() then
+    elseif speed >= (target_t[i] - INDUCTION_THRESHOLD) and
+        not turbine.getInductorEngaged() then
       print(string.format('Turbine %d - engaged', i))
-      t[i].setInductorEngaged(true)
-    elseif speed < (target_t[i] - INDUCTION_THRESHOLD) and t[i].getInductorEngaged() then
+      turbine.setInductorEngaged(true)
+    elseif speed < (target_t[i] - INDUCTION_THRESHOLD) and
+        turbine.getInductorEngaged() then
       print(string.format('Turbine %d - disengaged', i))
-      t[i].setInductorEngaged(false)
+      turbine.setInductorEngaged(false)
     end
   end
 end
